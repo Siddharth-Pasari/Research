b2b1372adf2e5690013496cfb4e137345d80ebba
xD-Crakkman <147352341+Siddharth-Pasari@users.noreply.github.com>
2024-07-30 15:18:47 -0400

    new

---
+++ Profilometry_1/Profilometry Program/main program/dragrectangle.py
@@ -0,0 +1,255 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import numpy as np
+import math
+import pandas as pd
+import openpyxl
+from openpyxl.styles import Font
+
+title="TITLE"
+def update_excel_with_data(data_measurements, file_path, num):
+    print(data_measurements, num)
+
+    # Create a DataFrame with specified column names
+    df = pd.DataFrame(data_measurements, columns=['Num', 'Top', 'Bottom', 'Difference'])
+
+    with pd.ExcelWriter(file_path, mode='a', engine='openpyxl', if_sheet_exists='overlay') as writer:
+        try:
+            startrow = writer.sheets['Sheet1'].max_row
+        except KeyError:
+            startrow = 0
+
+        if (num-1) % ftnumber == 0 and startrow != 0:  # Assuming ftnumber is defined somewhere else
+            startrow += 1
+
+        if num == 1:
+            # Write the title above the headers if this is the first set of measurements
+            ws = writer.sheets['Sheet1']    
+            ws.insert_rows(startrow, amount=1)  # Insert a blank row above the title
+            ws.cell(row=startrow+1, column=1, value=title).font = Font(bold=True)  # Write the title and make it bold
+            headers = ['Num', 'Top', 'Bottom', 'Difference']
+            # Increment startrow so headers will be below the title
+            startrow += 1
+
+        else:
+            headers = False
+        
+        # Write the DataFrame to the sheet at the updated starting row
+        df.to_excel(writer, sheet_name='Sheet1', startrow=startrow, index=False, header=headers)
+
+def plot_2d_slice(height_values, max_val, excel_path):
+    """
+    Plots a 2D representation of a data slice given height values.
+    A vertical line and the y coordinate of the line associated with the current x-coordinate
+    of the mouse are displayed as the mouse moves. Clicking records the y-value.
+
+    Parameters:
+    - height_values: 1D array of height values
+    """
+    x_values = np.arange(len(height_values))
+
+    fig, ax = plt.subplots()
+    ax.plot(x_values, height_values, 'o-')
+
+    # Set labels and title
+    ax.set(xlabel='X', ylabel='Height',
+           title='2D Representation of Data Slice')
+    ax.grid()
+
+    # Marker and line which will follow the mouse
+    marker, = ax.plot([0], [height_values[0]], marker='o', color="red", zorder=5)
+    cursor_line = ax.axvline(x=0, color='red', linestyle='--', lw=1)
+
+    # Text annotation for displaying the y-value
+    y_value_annotation = ax.annotate('', xy=(0, 0), xytext=(10, 10),
+                                     textcoords="offset points",
+                                     bbox=dict(boxstyle="round4", fc="cyan", ec="black", lw=1))
+
+    # Variable to hold the y-value
+    bottom_val = None
+
+    def on_move(event):
+        if not event.inaxes:
+            return
+        x, y = event.xdata, event.ydata
+        nearest_x_index = np.clip(np.searchsorted(x_values, x), 1, len(x_values) - 1)
+        x0, x1 = x_values[nearest_x_index - 1], x_values[nearest_x_index]
+        y0, y1 = height_values[nearest_x_index - 1], height_values[nearest_x_index]
+        distance_to_x0 = abs(x - x0)
+        distance_to_x1 = abs(x - x1)
+        index = nearest_x_index - 1 if distance_to_x0 < distance_to_x1 else nearest_x_index
+
+        # Update the position of the marker and the vertical line
+        marker.set_data([x_values[index]], [height_values[index]])
+        cursor_line.set_xdata([x_values[index]])  # Provide a sequence with a single value
+
+        # Update the annotation text and position
+        y_value_annotation.set_text(f'({x_values[index]:.2f}, {height_values[index]:.2f})')
+        y_value_annotation.xy = (x_values[index], height_values[index])
+
+        plt.draw()
+
+    def on_click(event):
+        global number
+        number += 1
+        if not event.inaxes:
+            return
+        nonlocal bottom_val
+        bottom_val = height_values[np.clip(np.searchsorted(x_values, event.xdata), 1, len(x_values) - 1) - 1]
+
+        difference = max_val - bottom_val
+
+        data_measurements = [(number, max_val, bottom_val, difference)]
+
+        update_excel_with_data(data_measurements, excel_path, number)
+
+        plt.close()
+
+    fig.canvas.mpl_connect('motion_notify_event', on_move)
+    fig.canvas.mpl_connect('button_press_event', on_click)
+
+    ax.autoscale_view()  # Auto rescale the view after adding the marker
+    plt.show()
+
+
+class DragRectangle:
+
+    def __init__(self, ax, x_values, y_values, data, path, num, ftnum, titlet):
+        global number, ftnumber, title
+        self.ax = ax
+        self.x_values = x_values
+        self.y_values = y_values
+        self.data = data
+        self.path = path
+        self.num = num
+        number=num
+        self.ftnum=ftnum
+        ftnumber=ftnum
+        title=titlet
+        self.rect = Rectangle((0, 0), 0, 0, linewidth=1, edgecolor='r', facecolor='none')
+        self.is_dragging = False
+        self.press_event = None
+        self.selected_indices = []
+
+    def connect(self):
+        self.cidpress = self.ax.figure.canvas.mpl_connect('button_press_event', self.on_press)
+        self.cidrelease = self.ax.figure.canvas.mpl_connect('button_release_event', self.on_release)
+        self.cidmotion = self.ax.figure.canvas.mpl_connect('motion_notify_event', self.on_motion)
+
+    def on_press(self, event):
+        if event.inaxes != self.ax:
+            return
+
+        if event.button == 3:  # Right-click event
+            self.on_right_click(event)
+            return
+        
+        if event.button == 2:
+            self.on_middle_click(event)
+            return
+
+        self.press_event = event
+        self.rect.set_width(0)
+        self.rect.set_height(0)
+        self.rect.set_xy((event.xdata, event.ydata))
+        self.ax.add_patch(self.rect)
+        self.is_dragging = True
+
+    def on_right_click(self, event):
+        global number
+        number=number+1
+        data_measurements = [(number, "N/A", "N/A", "N/A")]
+
+        update_excel_with_data(data_measurements, self.path, number)
+
+    def on_middle_click(self, event):
+        global number
+        number=number-1
+        # Load the workbook and the active worksheet
+        file_path = self.path
+        workbook = openpyxl.load_workbook(file_path)
+        sheet = workbook.active
+
+        # Find the last row with data in the Excel sheet
+        df = pd.read_excel(file_path)
+        last_row_index = df.last_valid_index()
+
+        # If there is at least one row, delete the last one
+        if last_row_index is not None:
+            sheet.delete_rows(last_row_index + 2)  # +2 because DataFrame index is 0-based and Excel row numbers are 1-based; add 1 more to move past the header
+
+        # Save the modified workbook
+        print("deleted")
+        workbook.save(file_path)
+
+    def on_motion(self, event):
+        if not self.is_dragging or event.inaxes != self.ax:
+            return
+
+        width = event.xdata - self.press_event.xdata
+        height = event.ydata - self.press_event.ydata
+
+        self.rect.set_width(width)
+        self.rect.set_height(height)
+        self.ax.figure.canvas.draw()
+
+    def on_release(self, event):
+        if not self.is_dragging or event.inaxes != self.ax:
+            return
+
+        self.is_dragging = False
+        self.ax.figure.canvas.draw()
+
+        # Get indices of selected area
+        x0, y0 = self.press_event.xdata, self.press_event.ydata
+        x1, y1 = event.xdata, event.ydata
+
+        if x0 > x1:
+            x0, x1 = x1, x0
+        if y0 > y1:
+            y0, y1 = y1, y0
+
+        x_indices = [int(i) for i in range(math.ceil(x0), math.floor(x1))]
+        y_indices = [int(i) for i in range(math.ceil(y0), math.floor(y1))]
+
+        # Get values within selected area
+        self.selected_indices = []
+        for j in x_indices:
+            row_values = [self.data[j, i] for i in y_indices]
+            self.selected_indices.append(row_values)
+
+        slicelist=[]
+        
+        self.findImportantValues()
+
+    def findImportantValues(self):
+        if not self.selected_indices:
+            return np.nan, np.nan  # Return NaN values if selected_indices is empty
+
+        # finds the row containing the peak
+        max_value = float('-inf')
+        max_list = None
+        for sublist in self.selected_indices:
+            sublist_max = max(sublist)
+            if sublist_max > max_value:
+                max_value = sublist_max
+                max_list = sublist
+        
+        if max_list is None:
+            return np.nan, np.nan  # Return NaN values if max_list is None
+
+        max_index = np.argmax(max_list)
+        
+        '''# Convert list to numpy array for easier manipulation and finds bottom based on a slope threshold
+        slicearr = np.array(max_list)
+        differences = np.diff(slicearr)
+        threshold = 0.005  # Adjust the threshold as needed
+
+        if np.any(differences > threshold):
+            bottom_index = np.argmax(differences > threshold)
+            bottom_value = slicearr[bottom_index]
+        else:
+            bottom_value = np.nan'''
+        
+
+        plot_2d_slice(max_list, max_value, self.path)
---
+++ Profilometry_1/Profilometry Program/main program/opdx_plotter.py
@@ -0,0 +1,191 @@
+import tkinter as tk
+from tkinter import filedialog
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+import cv2
+import dragrectangle
+import re
+import math
+from opdx_reader import DektakLoad
+
+print("-----------------------------------------------------------------------")
+
+def level(heightmap):
+    # Calculate the slope on the x-axis using the lower height of the left and right columns
+    left_column_min = np.min(heightmap[:, 0])
+    right_column_min = np.min(heightmap[:, -1])
+    x_slope = (right_column_min - left_column_min) / heightmap.shape[1]
+
+    # Divide each column by the x-axis slope
+    for col_index in range(heightmap.shape[1]):
+        heightmap[:, col_index] -= left_column_min + col_index * x_slope
+
+    # Calculate the slope on the y-axis using the lower height of the top and bottom rows
+    top_row_min = np.min(heightmap[0, :])
+    bottom_row_min = np.min(heightmap[-1, :])
+    y_slope = (bottom_row_min - top_row_min) / heightmap.shape[0]
+
+    # Divide each row by the y-axis slope
+    for row_index in range(heightmap.shape[0]):
+        heightmap[row_index, :] -= top_row_min + row_index * y_slope
+
+    return heightmap
+    
+def open_file():
+    global file_path
+    if not plt.fignum_exists(1):
+        file_path = filedialog.askopenfilename(filetypes=[("OPDX files", "*.opdx")])
+        if file_path:
+            ftnum=16
+            is_empty = not ftnumt.get()
+            if not is_empty:
+                try:
+                    ftnum = int(ftnumt.get())
+                except:
+                    pass
+            is_empty = not startnum.get()
+            if not is_empty:
+                try:
+                    process_file(file_path, int(startnum.get()), ftnum)
+                except:
+                    process_file(file_path,0,ftnum)
+            else:
+                process_file(file_path,0,ftnum)
+    else:
+        print("Close current plot before opening a new one!")
+
+def open_file2():
+    global path
+    if not plt.fignum_exists(1):
+        path = filedialog.askopenfilename(filetypes=[("excel files", "*.xlsx")])
+
+def read_opdx(file_path):
+    loader = DektakLoad(file_path)
+    x, y, z = loader.get_data_2D()
+    metadata = loader.get_metadata()
+    return y, x, z.T, metadata # x and y swapped because of the TRANSPOSE
+
+def process_file(file_path,num=0,ftnum=16):
+
+    def format_coord(x,y):
+        # properly print some coordinates and other useful info
+        return f'{x:.2f},{y:.2f}, raw={data[math.floor(x), math.floor(y)]:0.4f}' #, uM={data_transpose[round(y), round(x)]:0.4f}'
+
+    global data_x, data_y
+
+    # opdx file
+    x, y, data_raw, metadata = read_opdx(file_path)
+
+    if level_var.get() == 1:
+        data = level(data_raw)
+
+    minimum = data_raw.min()
+    data = ((data_raw - minimum) * 1e6) # now in microns instead of meters
+    data_y, data_x = data_raw.shape
+
+    x_uM = x.max()
+    y_uM = y.max()
+
+    aspect_ratio = (data_y/data_x) * (y_uM/x_uM)
+
+    data_transpose = data.T
+
+    tdata = data
+
+    if rotate_var.get() == 1:
+
+        data_transpose = np.rot90(data_transpose, 2)
+
+        tdata=np.rot90(data, 2)
+
+    # yes i only wrote this to look like the profilmonline colormap since i think it looks cool
+    image = cv2.imread("Colormap.png")
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    colors = []
+    for row in reversed(image_rgb):
+        for color in row:
+            colors.append(color / 255.0)  # Normalize the colors to range [0, 1]
+    custom_cmap = LinearSegmentedColormap.from_list('custom_colormap', colors)
+
+    # Plotting the data with a custom colormap
+    plt.imshow(data_transpose, cmap=custom_cmap, aspect=aspect_ratio) # rotate
+    plt.colorbar()  # Add a color bar for reference
+    #fig=plt.figure()
+
+    # create custom tick values
+    x_ticks = np.linspace(0, data_x, int(x_uM / 500)) 
+    y_ticks = np.linspace(0, data_y, int(y_uM / 500)) 
+
+    # create custom tick labels
+    x_tick_labels = ['{:.0f}'.format(x * (y_uM / data_x)) for x in x_ticks]  # due to the transpose, switch x_uM and y_uM here
+    y_tick_labels = ['{:.0f}'.format(y * (x_uM / data_y)) for y in y_ticks]  # due to the transpose, switch x_uM and y_uM here
+
+    # set ticks
+    plt.yticks(x_ticks, x_tick_labels)
+    plt.xticks(y_ticks, y_tick_labels)
+
+    # add labels to the axes
+    plt.xlabel('X-Distance (µM)')
+    plt.ylabel('Y-Distance (µM)')
+
+    # interactivify the plot
+    # mplcursors.cursor(hover=True).connect("add", lambda sel: sel.annotation.set_text(f"Value: {data[sel.target.index]}"))
+
+    # Create the draggable rectangle
+    ax = plt.gca()
+    x_values = np.linspace(0, data_x, data_x + 1)
+    y_values = np.linspace(0, data_y, data_y + 1)
+
+    ax.format_coord=format_coord
+    
+
+    dr = dragrectangle.DragRectangle(ax, x_values, y_values, tdata, path, num, ftnum, str(titlet.get()))
+    dr.connect()
+
+    # open plot
+    plt.show()
+
+    print(f"Plotted numpy array as image with colormap and scale")
+
+root = tk.Tk()
+root.title("OPDX File Processor")
+
+startnum = tk.Entry(root)
+startnum.insert(0, "last number recorded (0)")
+startnum.pack()
+
+ftnumt= tk.Entry(root)
+ftnumt.insert(0, "# of features (16)")
+ftnumt.pack()
+
+titlet= tk.Entry(root)
+titlet.insert(0, "name of file (only if starting new)")
+titlet.pack()
+
+level_var = tk.IntVar()
+
+level_check = tk.Checkbutton(root, text="Levelling", variable=level_var, onvalue=1, offvalue=0)
+level_check.pack()
+
+rotate_var = tk.IntVar()
+
+rotate_check = tk.Checkbutton(root, text="Rotate 180°", variable=rotate_var, onvalue=1, offvalue=0)
+rotate_check.pack()
+
+file_path_label = tk.Button(root, text="Open Excel File", command=open_file2)
+file_path_label.pack()
+
+btn_open1 = tk.Button(root, text="Open OPDX File", command=open_file)
+btn_open1.pack()
+
+btn_open = tk.Button(root, text="Exit Program", command=exit)
+btn_open.pack()
+
+info = tk.Label(root, text = "\n1. Open an excel file to log data to using the button\n\n2. Choose the OPDX file given to you by the DektakXT profilometer to plot\n\n3. Drag a rectangle around a feature, and then click the feature's bottom\n value as seen on the 2d graph. This will log both the top, bottom\nand net height of the feature to the provided excel sheet\n(see documentation video)\n\n4. To log an 'N/A' value to the provided excel sheet, right click the heatmap\n\n5. To delete a value set, middle click the heatmap")
+info.pack()
+
+# Start the Tkinter event loop
+root.mainloop()
+
+print("Closing application...")
